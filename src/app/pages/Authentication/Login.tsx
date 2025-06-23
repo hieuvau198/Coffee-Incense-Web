ac6e99c5cd7e2b642c2678bd1353adee914f0da9
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-06-23 15:06:45 +0700

    fix login left background

--- src/app/pages/Authentication/Login.tsx
+++ src/app/pages/Authentication/Login.tsx
@@ -22,7 +22,9 @@ const Login: React.FC = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center" 
           style={{ 
-            backgroundImage: "url('https://images.unsplash.com/photo-1610889556528-9a770e32642f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80')",
+            // backgroundImage: "url('https://images.unsplash.com/photo-1610889556528-9a770e32642f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80')",
+            backgroundImage: "url('https://i.postimg.cc/v8gFY1Mb/ebc-login-left-background.png')",
+
             filter: "brightness(0.8)"
           }}
         >
